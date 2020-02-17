3f423be447be42819a5ad49a79f365cf3d822ce1
Zoe Tsekas <zoetsekas@live.co.uk>
2020-02-17 17:02:48 +0000

    fit_generator deprecated

--- model/image_classification.py
+++ model/image_classification.py
@@ -10,17 +10,17 @@ from pathlib import Path
 
 def get_model():
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation=tf.nn.relu, input_shape=(150, 150, 3)),
         tf.keras.layers.MaxPooling2D(2, 2),
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.Conv2D(64, (3, 3), activation=tf.nn.relu),
         tf.keras.layers.MaxPooling2D(2, 2),
-        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.Conv2D(128, (3, 3), activation=tf.nn.relu),
         tf.keras.layers.MaxPooling2D(2, 2),
-        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.Conv2D(128, (3, 3), activation=tf.nn.relu),
         tf.keras.layers.MaxPooling2D(2, 2),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(3, activation='softmax')
+        tf.keras.layers.Dense(512, activation=tf.nn.relu),
+        tf.keras.layers.Dense(3, activation=tf.nn.softmax)
     ])
 
     model.compile(loss='categorical_crossentropy',
@@ -82,7 +82,7 @@ def run(model, log_dir, mode='default', epochs=100):
             # batch_size=20,
             class_mode='categorical')
 
-        fit_model = model.fit_generator(
+        fit_model = model.fit(
             train_generator,
             steps_per_epoch=1,  # 2000 images = batch_size * steps
             epochs=epochs,
